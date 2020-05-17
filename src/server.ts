edb44668bfed0411fe48332a5d3b784912b4c184
Destin <d.gandobak@gmail.com>
2020-05-17 14:12:05 +0200

    Environment variables pour la connection à la db avec dotenv. Validating environment variables avec envalid

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
+import 'dotenv/config';
 import App from './app';
 import PostController from './posts/posts.controller';
+import validateEnv from './utils/validateEnv';
+
+validateEnv();
 
 const app = new App([new PostController()], 8800);
 app.listen();
