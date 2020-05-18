e2d7cfdabab2fd910b8b80cc2cd7423f19a437b8
Destin <d.gandobak@gmail.com>
2020-05-18 14:17:52 +0200

    Fix bug: modifier un post

--- src/app.ts
+++ src/app.ts
@@ -24,13 +24,16 @@ class App {
 
     private connectToTheDatabase() {
         const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH } = process.env;
-        mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`, { 
+        
+        mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useFindAndModify: false
         })
-        .then(() => console.log('Connected'))
+        .then(() => console.log('Connected to Mongo'))
         .catch((err) => console.error(err));
     }
+
     /**
      * listen
      */
