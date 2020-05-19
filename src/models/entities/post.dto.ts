6ee8c2e39e55e861aed0a239ede0dd6472383b19
Destin <d.gandobak@gmail.com>
2020-05-19 13:11:03 +0200

    Bug compilation avec class-validator

--- src/models/entities/post.dto.ts
+++ src/models/entities/post.dto.ts
@@ -1,14 +1,14 @@
 import { IsString } from 'class-validator';
 
 class CreatePostDTO {
-    @IsString()
-    public author: string;
-
-    @IsString()
-    public content: string;
-
-    @IsString()
-    public title: string;
+    // @IsString()
+  public author: string;
+ 
+//   @IsString()
+  public content: string;
+ 
+//   @IsString()
+  public title: string;
 }
 
 export default CreatePostDTO;
