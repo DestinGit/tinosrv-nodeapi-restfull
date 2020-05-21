fe664478e4da709e3883df70872c404ac2ffb925
Destin <d.gandobak@gmail.com>
2020-05-21 18:35:10 +0200

    Gestion d'authentification

--- src/controllers/authentication/authentication.controller.ts
+++ src/controllers/authentication/authentication.controller.ts
@@ -3,22 +3,58 @@ import * as express from 'express';
 import validationMiddleware from '../../middleware/validation.middleware';
 import CreateUserDTO from '../../models/entities/user.dto';
 import CreateLogInDTO from '../../models/entities/login.dto';
-
+import userModel from '../../models/users.model';
+import * as bcrypt from 'bcrypt';
+import UserWithThatEmailAlreadyExistsException from '../../exceptions/UserWithThatEmailAlreadyExistsException';
+import WrongCredentialsException from '../../exceptions/WrongCredentialsException';
 
 export default class AuthenticationController implements Controller{
     path: String = '/auth';
     router: express.Router;
-
+    private user = userModel;
+    
     constructor() {
         this.initializeRoutes();    
     }
 
     private initializeRoutes() {
         this.router.post(`${this.path}/register`, validationMiddleware(CreateUserDTO), this.registration);
-        this.router.post(`${this.path}/login`,validationMiddleware(CreateLogInDTO), this.loggingIn)
+        this.router.post(`${this.path}/login`, validationMiddleware(CreateLogInDTO), this.loggingIn)
     }
 
-    private registration = (req: express.Request, res: express.Response, next: express.NextFunction) => {}
+    private registration = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        const userData: CreateUserDTO = req.body;
 
-    private loggingIn = (req: express.Request, res: express.Response, next: express.NextFunction) => {}
+        const userExist = await this.user.findOne({email: userData.email})
+        if (userExist) {
+            next(new UserWithThatEmailAlreadyExistsException(userData.email));
+        } else {
+            const hashedPwd = await bcrypt.hash(userData.password, 10);
+            const userDoc = await this.user.create({
+                ...userData,
+                password: hashedPwd
+            });
+
+            userDoc.password = undefined;
+            res.status(200).send(userDoc);
+        }
+    }
+
+    private loggingIn = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        const logInData: CreateLogInDTO = req.body;
+
+        const userData = await this.user.findOne({email: logInData.email});
+        if (userData) {
+            const hashedPwd = await bcrypt.hash(logInData.password, 10);
+            const doPasswordsMatch = await bcrypt.compare(logInData.password, userData.password);
+            if (doPasswordsMatch) {
+                userData.password = undefined;
+                res.status(200).send(userData);
+            } else {
+                next(new WrongCredentialsException());
+            }
+        } else {
+            next(new WrongCredentialsException());
+        }
+    }
 }
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import 'dotenv/config';
 import App from './app';
 import PostController from './controllers/posts/posts.controller';
 import validateEnv from './utils/validateEnv';
+import AuthenticationController from './controllers/authentication/authentication.controller';
 
 validateEnv();
 
-const app = new App([new PostController()],);
+const app = new App([new PostController(), new AuthenticationController()],);
 app.listen();
